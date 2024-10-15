ee2c6692ed71acd044ab2bd02505dad009fa0985
Hans Adrianus Hersiono <hans.hersiono@ccamatil.com>
2024-10-15 22:03:07 +0700

    Add new project

---
+++ DummyConsole1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DummyConsole1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! This is Dummy Console 1!");
+        }
+    }
+}
